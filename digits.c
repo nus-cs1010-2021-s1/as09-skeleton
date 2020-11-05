24f4f00a6088e71bffd697ef215e1882a6e0c3e9
Ooi Wei Tsang <weitsang@yahoo.com>
2020-11-05 11:01:03 +0800

    First commit

---
+++ digits.c
@@ -0,0 +1,31 @@
+/**
+ * CS1010 Semester 1 AY20/21
+ * Assignment 9: Digits
+ *
+ * TODO(by student): Brief description of the purpose 
+ * of this program.
+ *
+ * @file: digits.c
+ * @author: XXX (Group XXX)
+ */
+
+#include "cs1010.h"
+
+#define NCOLS 28
+#define NROWS 28
+#define WHITE '.'
+#define BLACK '#'
+#define K 5
+
+typedef struct {
+  // TODO
+} digit;
+
+typedef struct {
+  // TODO
+} neighbor;
+
+int main()
+{
+}
+
